cc5ea4ed629614212ea564f1189559193ca559b0
Kevin Kohler <kevin.kohler@penn-interactive.com>
2021-08-17 08:30:16 -0400

    made tag option (default false)

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ import { createAnnotations } from './createAnnotation'
 async function run() {
     
 
+    //Read our Github Token
     const githubToken =
         core.getInput('github_token') || process.env.GITHUB_TOKEN
     const ignore =
@@ -23,19 +24,33 @@ async function run() {
             .split(',')
             .map((x) => x.trim())
             .filter(Boolean) || []
+
+    //Get our Github Ref and Branch
     const GITHUB_REF = process.env.GITHUB_REF || ''
     const branch =
         core.getInput('branch') ||
         process.env.BRANCH ||
         GITHUB_REF.split('/').reverse()[0] ||
         'master'
+    
+    //Grab the version file location
     const versionPath = core.getInput('version_file') || 'version.json'
+
+    //If our version file doesn't exist, write an initial one.
     if (!fs.existsSync(versionPath)) {
-        fs.writeFileSync(versionPath, '0.0.0', 'utf8')
+        fs.writeFileSync(versionPath, '{"version":"0.0.0"}', 'utf8')
     }
+
+    //Grab prefix.  Unused by us currently but leaving it in
     const prefix = (core.getInput('prefix') || '').trim()
-    var versionjson = JSON.parse(fs.readFileSync(versionPath, 'utf8'))
+
+    //Read in our version file and parse it
+    var versionjson = JSON.parse(fs.readFileSync(versionPath, 'utf8'))   
+
+    //Since its JSON we need to grab out the version variable. 
     const version = versionjson["version"]    
+
+    //Determin our increment type, then use the inc function from the semver package to increment correctly.
     const incrementType = core.getInput('increment_type') || 'patch'    
     //TODO is there a better way to enforce correct increment type?
     const newVersion = inc(
@@ -48,9 +63,13 @@ async function run() {
     if (!newVersion) {
         throw new Error('could not bump version ' + version)
     }
+    
+    //Set our version to our new version and write the file back out to the file location
     versionjson["version"] = newVersion
     console.log('writing new version file')
     fs.writeFileSync(versionPath, JSON.stringify(versionjson), 'utf8')
+
+    //Look through files and replace version in lines after [bump] with the new version
     let linesReplaced: LineReplaced[] = []
     if (prefix) {
         console.log(`replacing version patterns below [bump if ${prefix}]`)
@@ -72,10 +91,14 @@ async function run() {
         })
         linesReplaced = res.linesReplaced
     }
-    const tagName = prefix ? prefix + '_' + newVersion : newVersion
+
+    //Grab a prefix tag if necessary.  for go we will often prefix tags with v since it uses that for modules.
+    const prefixTag = core.getInput('prefix_tag') || ''
+    const tagName = prefix ? prefixTag + prefix + '_' + newVersion : prefixTag + newVersion
     const tagMsg = `${capitalize(prefix) + ' '}Version ${newVersion} [skip ci]`
-    await Promise.all([
-        commit({
+
+    //Commit
+    await commit({
             USER_EMAIL: 'bump-version@version.com',
             USER_NAME: 'bump_version',
             GITHUB_TOKEN: githubToken,
@@ -83,12 +106,19 @@ async function run() {
             tagName,
             tagMsg,
             branch,
-        }),
-        createTag({
+        })
+
+    const shouldtagstring = core.getInput('tag') || 'false'
+    var shouldtag = shouldtagstring == "true" ? true : false
+
+    //Tag if shouldtag is true
+    if(shouldtag){
+        await createTag({
             tagName,
             tagMsg,
-        }),
-    ])
+        })
+    }
+
     console.log('setting output version=' + newVersion + ' prefix=' + prefix)
     await createAnnotations({ githubToken, newVersion: tagMsg, linesReplaced })
     core.setOutput('version', newVersion)
